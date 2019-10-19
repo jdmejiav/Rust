51cf668932c90574a745bfc7366cf8438121f5a4
jdmejiav <jdmejiav@eafit.edu.co>
2019-10-18 21:52:35 -0500

    implementación de la función fibonacci

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,8 +1,25 @@
+
 use std::io;
 
 fn main () {
+	
 	let mut fib :String = String::new();
+	println!("Ingrese un de fibonacci a calcular: ");
 	io::stdin().read_line(&mut fib).unwrap();
-	fib: u32 = fib.trim().parse().Ok().Expect("Hubo un error");
- 
+	let _fib: u64 = fib.trim().parse().ok().expect("Hubo un error");
+	let calc = fibonacci(_fib);
+	println!("El fibonacci de {}, es : {}",_fib,calc); 
+}
+
+
+
+fn fibonacci (n: u64) -> u64{
+	match n {
+		0=>0,
+		1=>1,
+		2=>1,
+		_=>fibonacci(n-1)+fibonacci(n-2),
+	}
+
+
 }
---
+++ fibonacci/target/debug/fibonacci.d
@@ -0,0 +1,1 @@
+/home/juandiego/Documentos/Repositorios/Rust/fibonacci/target/debug/fibonacci: /home/juandiego/Documentos/Repositorios/Rust/fibonacci/src/main.rs
